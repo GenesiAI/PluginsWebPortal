25e7b803a827a7b59bc334b592f06af098f083c1
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2023-07-17 19:56:15 +0200

    a bit of code cleanup

--- src/pages/YourPlugins/YourPlugins.tsx
+++ src/pages/YourPlugins/YourPlugins.tsx
@@ -1,24 +1,10 @@
-import { Box, Divider, Link, List, ListItem } from "@mui/material";
+import { List } from "@mui/material";
+import Instructions from "./Instructions";
 import PluginFooter from "./PluginFooter";
 import PluginHeader from "./PluginHeader";
 import Plugins from "./Plugins";
 
 const YourPlugins = () => {
-  const testSteps = [
-    "1. Copy the link",
-    <Link href="https://chat.openai.com/">2. Go to the plugin store</Link>,
-    "3. Press 'install unverified plugin'",
-    "4. Paste the link and press 'Find plugin'"
-  ];
-
-  const releaseSteps = [
-    <Link href="https://platform.openai.com/docs/plugins/review/submit-a-plugin-for-review#">
-      1. Open the release plugin bot
-    </Link>,
-    "2. Insert link and your data",
-    "3. Wait to be notified by email when OpenAI accept your plugin"
-  ];
-
   return (
     <>
       <List
@@ -33,36 +19,7 @@ const YourPlugins = () => {
         <Plugins />
         <PluginFooter />
       </List>
-      <Box
-        sx={{
-          marginTop: 1,
-          borderRadius: 3,
-          padding: 3
-        }}
-      >
-        <>
-          <List>
-            <ListItem>
-              <b>How to test:</b>
-            </ListItem>
-            {testSteps.map((step, index) => (
-              <ListItem key={index}>{step}</ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            <ListItem>
-              <b>How to release:</b>
-            </ListItem>
-            {releaseSteps.map((step, index) => (
-              <ListItem key={index}>{step}</ListItem>
-            ))}
-          </List>
-          <Link href="/support#runningPlugin">
-            <b> Discover more on how to test and release your plugin</b>
-          </Link>
-        </>
-      </Box>
+      <Instructions />
     </>
   );
 };
