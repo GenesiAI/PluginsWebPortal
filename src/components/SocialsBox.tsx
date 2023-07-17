492f8df76b14ed557d6b60c8d16ade0e5d152688
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2023-07-17 20:50:43 +0200

    fixed and hide contact form

--- src/components/SocialsBox.tsx
+++ src/components/SocialsBox.tsx
@@ -1,28 +1,14 @@
-import React from "react";
-import { Box } from "@mui/material";
 import { Twitter } from "@mui/icons-material";
-import { makeStyles } from "@mui/styles";
-import { Theme } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  socialIcons: {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: theme.spacing(3)
-  },
-  icon: {
-    margin: theme.spacing(1)
-  }
-}));
-
 const SocialsBox: React.FC = () => {
-  const classes = useStyles();
   return (
-    <Box className={classes.socialIcons}>
+    <Box margin={4}>
       {/* <Facebook className={classes.icon} /> */}
       <Link to="https://twitter.com/aipluginApp" target="_blank">
-        <Twitter className={classes.icon} />
+        <Twitter />
       </Link>
       {/* <LinkedIn className={classes.icon} />
              <Instagram className={classes.icon} /> */}
