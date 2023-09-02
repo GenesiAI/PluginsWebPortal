dac5e5ea5dc55966bb1ff3c6d8b36b43c3475506
ElSalvo96 <74384157+ElSalvo96@users.noreply.github.com>
2023-09-02 17:58:15 +0200

    fix: flyout position

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -2,35 +2,33 @@ import SpeedDial from "@mui/material/SpeedDial";
 import LogoFallback from "img/Logo.png";
 import useActions from "./useActions";
 
-const sx = { position: "absolute", bottom: 16, right: 16 };
+const sx = { position: "absolute", bottom: 25, right: 16 };
 
 const Flyout = () => {
   const { actions, photoUrl, handleClose, handleOpen, open, activeFlyout } =
     useActions();
   return (
-    <>
-      <SpeedDial
-        ariaLabel="User info tooltip"
-        sx={sx}
-        icon={
-          <img
-            className="aspect-square bg-no-repeat bg-contain w-full rounded-full animate-pulse"
-            src={photoUrl || LogoFallback}
-            srcSet={photoUrl || LogoFallback}
-            referrerPolicy="no-referrer"
-            alt=""
-            loading="lazy"
-            aria-hidden="true"
-          ></img>
-        }
-        hidden={!activeFlyout}
-        onClose={handleClose}
-        onOpen={handleOpen}
-        open={open}
-      >
-        {actions}
-      </SpeedDial>
-    </>
+    <SpeedDial
+      ariaLabel="User info tooltip"
+      sx={sx}
+      icon={
+        <img
+          className="aspect-square bg-no-repeat bg-contain w-full rounded-full animate-pulse"
+          src={photoUrl || LogoFallback}
+          srcSet={photoUrl || LogoFallback}
+          referrerPolicy="no-referrer"
+          alt=""
+          loading="lazy"
+          aria-hidden="true"
+        ></img>
+      }
+      hidden={!activeFlyout}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+    >
+      {actions}
+    </SpeedDial>
   );
 };
 
