0d6e01e186704ff104430054c951174e8d85937f
ElSalvo96 <74384157+ElSalvo96@users.noreply.github.com>
2023-09-20 18:54:08 +0200

    chore: remove navabar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 import Footer from "components/Footer";
 import Header from "components/Header";
 
-import PerfectScrollbar from "perfect-scrollbar";
-import "perfect-scrollbar/css/perfect-scrollbar.css";
-import { memo, useEffect, useRef } from "react";
+import { memo } from "react";
 import "./App.css";
 import AppRouter from "./AppRouter";
 import "./Tailwind.css";
 
 const App = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const ps = new PerfectScrollbar(ref.current!);
-    return () => ps.destroy();
-  }, []);
-
   return (
     <>
       <Header />
-      <div ref={ref} className="h-full relative">
-        <div
-          // className="flex justify-between flex-col h-full absolute min-h-screen"
-          className="h-full absolute"
-        >
-          <AppRouter />
-          <Footer />
-        </div>
+      <div className="flex justify-between flex-col min-h-screen overflow-y-auto">
+        <AppRouter />
+        <Footer />
       </div>
     </>
   );
