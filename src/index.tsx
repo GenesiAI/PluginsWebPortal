bcb60b3e1aec0c1351adbed7398d8a8faa6c8c16
ElSalvo96 <74384157+ElSalvo96@users.noreply.github.com>
2023-09-20 19:54:48 +0200

    chore: force console.log with us on dev tool

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,17 @@ import { BrowserRouter } from "react-router-dom";
 import theme from "theme";
 import App from "./App";
 
+// eslint-disable-next-line no-console
+console.log(
+  "%c Genesi AI",
+  "font-size: 80px; color: #6360FF; font-weight: bold; text-shadow: 2px 2px 4px #672B83;"
+);
+// eslint-disable-next-line no-console
+console.log(
+  "%c Build ChatGPT plugins in 2 minutes.",
+  "font-size: 40px; color: #6360FF; font-weight: bold; text-shadow: 2px 2px 4px #672B83;"
+);
+
 const container = document.getElementById("root")!;
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
