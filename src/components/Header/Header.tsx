5b4588d2fcc3a777416d968e0b5d6ea67dd2f7dd
ElSalvo96 <74384157+ElSalvo96@users.noreply.github.com>
2023-09-27 22:46:10 +0200

    fix: click on GenesiAI header
    fix: video in homepage (previous wrong remove)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <AppBar className="sticky top-0 grid w-screen grid-cols-[auto_auto] grid-rows-1 items-center whitespace-nowrap bg-white px-12 py-3 shadow-[0_2px_8px_0px_rgba(0,0,0,0.3)] md:grid-cols-[auto_minmax(0,1fr)_auto]">
       <Title />
       <HeaderXs />
-      <div className="col-span-3 col-start-1 row-start-1 hidden justify-center gap-4 md:flex">
+      <div className="-z-10 col-span-3 col-start-1 row-start-1 hidden justify-center gap-4 md:flex">
         <PrintLinks />
       </div>
       <div className="col-start-3 row-start-1 hidden justify-self-end md:block">
