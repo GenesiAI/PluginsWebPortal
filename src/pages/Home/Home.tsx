ad7b02436e5623201a7772192f478a5f5af152a0
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2023-11-17 19:58:57 +0100

    Goodbye my dear project

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import FeatureSection from "./FeatureSection";
@@ -8,6 +9,28 @@ import WhatSection from "./WhatSection";
 const Home: React.FC = () => {
   return (
     <div>
+      <Typography
+        variant="h2"
+        align="center"
+        sx={{
+          fontSize: { xs: "1.3rem", sm: "1.3rem" },
+          margin: { xs: "1rem 0", sm: "2rem 0" },
+          fontFamily: "Tahoma",
+          fontWeight: "bold",
+          color: "red"
+        }}
+      >
+        Genesi has been closed on november 2023, use{" "}
+        <a
+          href="https://openai.com/blog/introducing-gpts"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="OpenAI GPTs"
+          style={{ color: "blue", textDecoration: "underline" }}
+        >
+          OpenAI GPTs
+        </a>
+      </Typography>
       <HeroSection />
       <WhatSection />
       <FeatureSection />
